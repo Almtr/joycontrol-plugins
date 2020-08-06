b4a45313b8c1a1534eb492c0ba2866bf52b5dd32
almtr <almtr-0906@hotmail.co.jp>
2020-08-07 08:30:55 +0900

    Added the "GetBerry" plugin

---
+++ pokemon-swsh/TimeSkipGlitch/GetBerry.py
@@ -0,0 +1,33 @@
+import logging
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+from TimeSkipBasePlugin import TimeSkipBasePlugin
+
+logger = logging.getLogger(__name__)
+
+class GetBerry(TimeSkipBasePlugin):
+    async def run(self):
+        logger.info('Get Berry Plugin loaded!')
+
+        lap = 1
+        while True:
+            logger.info(f'{lap} lap')
+            lap += 1
+
+            # Talk to clerk
+            await self.button_push('a')
+            await self.wait(0.8)
+            await self.button_push('a')
+            await self.wait(0.8)
+            await self.button_push('a')
+            await self.wait(0.8)
+
+            # Close the all messages
+            for _ in range(20):
+                await self.button_push('b')
+                await self.wait(0.3)
+
+            await self.change_year()
+            await self.wait(1.0)
