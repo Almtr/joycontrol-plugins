c9f75c804b4d02c8b1018f267340a3a6b633c48f
almtr <almtr-0906@hotmail.co.jp>
2020-08-06 21:39:34 +0900

    Divided AutoLotoID into two files.

---
+++ pokemon-swsh/TimeSkipGlitch/TimeSkipBasePlugin.py
@@ -0,0 +1,65 @@
+import logging
+from JoycontrolPlugin import JoycontrolPlugin
+
+logger = logging.getLogger(__name__)
+
+class TimeSkipBasePlugin(JoycontrolPlugin):
+    async def change_year(self):
+        # Open the "Home"
+        await self.button_push('home')
+        await self.wait(0.8)
+
+        # Open the "Home > System Settings"
+        await self.button_push('down')
+        await self.wait(0.1)
+        await self.button_push('right', press_time_sec=0.8)
+        await self.wait(0.1)
+        await self.button_push('left')
+        await self.wait(0.1)
+        await self.button_push('a')
+        await self.wait(0.8)
+
+        # Open the "Home > System Settings > System"
+        await self.button_push('down', press_time_sec=1.5)
+        await self.wait(0.3)
+        await self.button_push('a')
+        await self.wait(0.3)
+
+        # Open the "Home > System Settings > System > Date and Time"
+        await self.button_push('down', press_time_sec=0.7)
+        await self.wait(0.1)
+        await self.button_push('a')
+        await self.wait(0.5)
+
+        # Open the "Home > System Settings > System > Date and Time > Date and Time"
+        await self.button_push('down', press_time_sec=0.5)
+        await self.wait(0.1)
+        await self.button_push('a')
+        await self.wait(0.3)
+
+        # Set next year
+        await self.button_push('up')
+        await self.wait(0.1)
+        await self.button_push('right', press_time_sec=1.0)
+        await self.wait(0.1)
+        await self.button_push('a')
+        await self.wait(0.1)
+
+        await self.button_push('a')
+        await self.wait(0.1)
+
+        # Set previous year
+        await self.button_push('left', press_time_sec=1.0)
+        await self.wait(0.1)
+        await self.button_push('down')
+        await self.wait(0.1)
+        await self.button_push('right', press_time_sec=1.0)
+        await self.wait(0.1)
+        await self.button_push('a')
+        await self.wait(0.1)
+
+        # Reopen the game.
+        await self.button_push('home')
+        await self.wait(1.0)
+        await self.button_push('a')
+        await self.wait(0.5)
