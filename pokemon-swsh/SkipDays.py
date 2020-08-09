d94c92e8115cb8fd3bf193e72ef061cf8b3560df
Zobio <64645053+Zobio@users.noreply.github.com>
2020-08-10 01:44:16 +0900

    Add files via upload

---
+++ pokemon-swsh/SkipDays.py
@@ -0,0 +1,71 @@
+import logging
+from JoycontrolPlugin import JoycontrolPlugin
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+logger = logging.getLogger(__name__)
+
+class SkipDays(JoycontrolPlugin):
+    async def skip_days(self):
+
+        daysLimitStr = input()
+        daysLimit = int(daysLimitStr)
+        daysNow = 0
+        daysOfMonth = 0
+        perCount = 0
+        
+        while daysNow < daysLimit:
+         if daysOfMonth != 30:
+           await self.button_push('a')
+           await self.wait(0.25)
+           await self.button_push('left')
+           await self.wait(0.035)
+           await self.button_push('left')
+           await self.wait(0.035)
+           await self.button_push('left')
+           await self.wait(0.035)
+           await self.button_push('up')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.2)
+           daysNow += 1
+           daysOfMonth += 1
+        
+         else:
+           await self.button_push('a')
+           await self.wait(0.25)
+           await self.button_push('left')
+           await self.wait(0.035)
+           await self.button_push('left')
+           await self.wait(0.035)
+           await self.button_push('left')
+           await self.wait(0.035)
+           await self.button_push('up')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.035)
+           await self.button_push('a')
+           await self.wait(0.25)
+           daysOfMonth = 0
+        
+         if int(daysNow / daysLimit * 100) > perCount:
+           logger.info(str(int(daysNow / daysLimit * 100)) + '% (' + str(daysNow) + 'days) finished')
+           perCount = int(daysNow / daysLimit * 100)
+
+
+    async def run(self):
+        logger.info('Get Feathers Plugin loaded!')
+        print('\nEnter the number of days you want')
+        await self.skip_days()
