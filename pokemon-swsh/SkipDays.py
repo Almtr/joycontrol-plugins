620bae6b03bbd02ae9b764a1fe4d5630aa685c05
almtr <almtr-0906@hotmail.co.jp>
2020-08-10 21:16:01 +0900

    Fixed indent width to 4

--- pokemon-swsh/SkipDays.py
+++ pokemon-swsh/SkipDays.py
@@ -14,31 +14,31 @@ class SkipDays(JoycontrolPlugin):
         
         await self.wait(1)
         while daysNow < daysLimit:
-         if daysOfMonth != 30:
-           daysNow += 1
-           daysOfMonth += 1
-         else:
-           daysOfMonth = 0
+            if daysOfMonth != 30:
+                daysNow += 1
+                daysOfMonth += 1
+            else:
+                daysOfMonth = 0
 
-         await self.button_push('a')
-         await self.wait(0.5)
-         await self.button_push('left')
-         await self.wait(0.055)
-         await self.button_push('left')
-         await self.wait(0.055)
-         await self.button_push('left')
-         await self.wait(0.055)
-         await self.button_push('up')
-         await self.wait(0.055)
-         await self.button_push('right', press_time_sec = 0.55)
-         await self.wait(0.055)
-         await self.button_push('a')
-         await self.wait(0.3)
+            await self.button_push('a')
+            await self.wait(0.5)
+            await self.button_push('left')
+            await self.wait(0.055)
+            await self.button_push('left')
+            await self.wait(0.055)
+            await self.button_push('left')
+            await self.wait(0.055)
+            await self.button_push('up')
+            await self.wait(0.055)
+            await self.button_push('right', press_time_sec = 0.55)
+            await self.wait(0.055)
+            await self.button_push('a')
+            await self.wait(0.3)
         
-         if int(daysNow / daysLimit * 100) > perCount:
-           logger.info(str(int(daysNow / daysLimit * 100)) + '% (' + str(daysNow) + 'days) finished')
-           perCount = int(daysNow / daysLimit * 100)
-           
+            if int(daysNow / daysLimit * 100) > perCount:
+                logger.info(str(int(daysNow / daysLimit * 100)) + '% (' + str(daysNow) + 'days) finished')
+                perCount = int(daysNow / daysLimit * 100)
+
         await self.button_push('home')
         await self.wait(1)
         await self.button_push('a')
