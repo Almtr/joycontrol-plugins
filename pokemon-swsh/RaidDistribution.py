f0fa4182a6c99588cbd5149bb6b082134d5bcb9c
Zobio <64645053+Zobio@users.noreply.github.com>
2020-08-10 22:05:41 +0900

    Add files via upload

---
+++ pokemon-swsh/RaidDistribution.py
@@ -0,0 +1,191 @@
+import logging
+from JoycontrolPlugin import JoycontrolPlugin
+
+logger = logging.getLogger(__name__)
+
+class RaidDistribution(JoycontrolPlugin):
+    async def raid_distribution(self):
+
+        linkCode = self.options[0]
+        num = 0
+        count = 0
+
+        while True:
+         await self.button_push('a')
+         await self.wait(1)
+         await self.button_push('a')
+         await self.wait(20)
+         await self.button_push('a')
+         await self.wait(10)
+         await self.button_push('y')
+         await self.wait(1)
+         await self.button_push('plus')
+         await self.wait(20)
+         await self.button_push('b')
+         await self.wait(0.5)
+         await self.button_push('b')
+         await self.wait(0.5)
+         await self.button_push('a')
+         await self.wait(4)
+         await self.button_push('plus')
+         await self.wait(1)
+         await self.button_push('down')
+         await self.wait(0.3)
+         await self.button_push('down')
+         await self.wait(0.3)
+         await self.button_push('down')
+         await self.wait(0.3)
+         for num in range(8):
+          if linkCode[num] == '0':
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+          elif linkCode[num] == '1':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('left', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '2':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '3':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('right', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)         
+          elif linkCode[num] == '4':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('left', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '5':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '6':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('right', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '7':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('left', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '8':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+          elif linkCode[num] == '9':
+           await self.button_push('up', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('right', press_time_sec = 0.1)
+           await self.wait(0.5)
+           await self.button_push('a', press_time_sec = 0.1)
+           await self.wait(0.5)
+           if num != 7:
+            await self.button_push('down', press_time_sec = 0.1)
+            await self.wait(0.5)
+           
+         await self.button_push('plus')
+         await self.wait(1)
+         await self.button_push('a')
+         await self.wait(1)
+         await self.button_push('a')
+         await self.wait(60)
+         await self.button_push('up')
+         await self.wait(0.5)
+         await self.button_push('a')
+         await self.wait(0.7)
+         await self.button_push('a')
+         await self.wait(0.7)
+         await self.button_push('a')
+         await self.wait(0.7)
+         await self.button_push('a')
+         await self.wait(15)
+         await self.button_push('home')
+         await self.wait(1.5)
+         await self.button_push('x')
+         await self.wait(0.5)
+         await self.button_push('a')
+         await self.wait(5)
+         count += 1
+         logger.info(str(count) + 'cycle finished.')        
+       
+       
+    async def run(self):
+        logger.info('Raid Distribution Plugin loaded!')
+        await self.raid_distribution()
