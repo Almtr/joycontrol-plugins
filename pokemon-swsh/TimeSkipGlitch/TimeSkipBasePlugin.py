5bf5c1c8002f008bed6c36c88317efee044b8b6d
Almtr <almtr-0906@hotmail.co.jp>
2020-08-18 05:53:19 +0900

    Split "Date and Time" settings into methods.

--- pokemon-swsh/TimeSkipGlitch/TimeSkipBasePlugin.py
+++ pokemon-swsh/TimeSkipGlitch/TimeSkipBasePlugin.py
@@ -4,7 +4,8 @@ from JoycontrolPlugin import JoycontrolPlugin
 logger = logging.getLogger(__name__)
 
 class TimeSkipBasePlugin(JoycontrolPlugin):
-    async def change_year(self):
+
+    async def open_date_and_time_settings(self):
         # Open the "Home"
         await self.button_push('home')
         await self.wait(0.8)
@@ -36,6 +37,9 @@ class TimeSkipBasePlugin(JoycontrolPlugin):
         await self.wait(0.1)
         await self.button_push('a')
         await self.wait(0.3)
+
+    async def change_year(self):
+        await self.open_date_and_time_settings()
 
         # Set next year
         await self.button_push('up')
@@ -63,52 +67,26 @@ class TimeSkipBasePlugin(JoycontrolPlugin):
         await self.wait(1.0)
         await self.button_push('a')
         await self.wait(0.5)
-    async def change_days(self,num):
-        # Open the "Home"
-        await self.button_push('home')
-        await self.wait(0.8)
 
-        # Open the "Home > System Settings"
-        await self.button_push('down')
-        await self.wait(0.1)
-        await self.button_push('right', press_time_sec=0.8)
-        await self.wait(0.1)
-        await self.button_push('left')
-        await self.wait(0.1)
-        await self.button_push('a')
-        await self.wait(0.8)
 
-        # Open the "Home > System Settings > System"
-        await self.button_push('down', press_time_sec=1.5)
-        await self.wait(0.3)
-        await self.button_push('a')
-        await self.wait(0.3)
+    async def change_days(self, days):
+        await self.open_date_and_time_settings()
 
-        # Open the "Home > System Settings > System > Date and Time"
-        await self.button_push('down', press_time_sec=0.6)
+        # Update days
+        await self.button_push('a')
         await self.wait(0.1)
         await self.button_push('a')
-        await self.wait(0.5)
+        await self.wait(0.1)
 
-        # Open the "Home > System Settings > System > Date and Time > Date and Time"
-        await self.button_push('down', press_time_sec=0.7)
-        await self.wait(0.1)
-        await self.button_push('a')
-        await self.wait(0.3)
-
-        # Set next day
-        await self.button_push('a')
-        await self.wait(0.1)
-        await self.button_push('a')
-        await self.wait(0.1)
-        if num >= 0 :
-            for _ in range(num) :
+        if days >= 0:
+            for _ in range(days):
                 await self.button_push('up')
                 await self.wait(0.1)
-        else :
-            for _ in range(num * -1) :
+        else:
+            for _ in range(days * -1):
                 await self.button_push('down')
                 await self.wait(0.1)
+
         await self.button_push('right', press_time_sec=0.5)
         await self.wait(0.1)
         await self.button_push('a')
