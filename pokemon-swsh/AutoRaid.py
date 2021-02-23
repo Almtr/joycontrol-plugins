405636d16c8e0bb34bf66d2952795b365ae0ec45
Almtr <almtr-0906@hotmail.co.jp>
2021-02-23 18:35:08 +0900

    Fixed to select "Don't Invite Others".
    Due to an update on December 22, 2020, the Bluetooth connection was lost when "Invite Others" was selected in Max Raid Battle.

--- pokemon-swsh/AutoRaid.py
+++ pokemon-swsh/AutoRaid.py
@@ -8,15 +8,39 @@ class AutoRaid(JoycontrolPlugin):
         logger.info('Auto Raid Plugin loaded!')
 
         while True:
-            await self.button_push('a') # Select the "Fight" button
-            await self.wait(0.1)
-            await self.button_push('up')
-            await self.wait(0.3)
-            await self.button_push('a') # Select the "Next" button
-            await self.wait(0.1)
             await self.button_push('a')
-            await self.wait(0.3)
-            await self.button_push('a')
-            await self.wait(0.1)
+            await self.wait(0.5)
+
+            # Before a raid battle, press the "Don't Invite Others" button.
+            # During a raid battle, press the "Pokemon" button.
             await self.button_push('up', press_time_sec=3.0)
             await self.wait(0.3)
+            await self.button_push('down')
+            await self.wait(0.3)
+            await self.button_push('a')
+            await self.wait(0.3)
+
+            # Cancel all when "Pokemon" is selected.
+            for _ in range(3):
+                await self.button_push('b')
+                await self.wait(0.3)
+            await self.button_push('up', press_time_sec=3.0)
+            await self.wait(0.3)
+
+            # Select the "Fight" Button or throw a wishing piece.
+            for _ in range(3):
+                await self.button_push('a')
+                await self.wait(0.5)
+                await self.button_push('down', press_time_sec=3.0)
+                await self.button_push('up')
+            
+            # Cancel all when "Switch Pokemon" is selected.
+            for _ in range(4):
+                await self.button_push('b')
+                await self.wait(0.3)
+
+            # Select the "Next" button after the battle is finished.
+            await self.button_push('down')
+            await self.wait(0.1)
+            await self.button_push('down')
+            await self.wait(0.1)
